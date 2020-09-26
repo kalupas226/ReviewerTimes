c969da927f063694cf52ee4287600435b8cbe26d
Kenta Aikawa <kenta.aikawa@sansan.com>
2020-09-26 12:40:37 +0900

    init

---
+++ RevewerTimes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RevewerTimes
+//
+//  Created by Aikawa Kenta on 2020/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RevewerTimes/RevewerTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevewerTimesApp.swift
+//  RevewerTimes
+//
+//  Created by Aikawa Kenta on 2020/09/26.
+//
+
+import SwiftUI
+
+@main
+struct RevewerTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
